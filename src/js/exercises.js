0aac8a6396cd6f75de2be7d9ee9f0eae9ca3fea3
Wallacy Caike <103141693+wallacycaike@users.noreply.github.com>
2022-12-17 20:13:14 -0300

    Iniciando a parte de mostrar no site os dados dos exercícios

--- src/js/exercises.js
+++ src/js/exercises.js
@@ -1,7 +1,38 @@
-// Testes do JSON
+const exercises = await fetch("../../exercises.json").then((response) =>
+  response.json()
+);
+dayjs.locale("pt-br");
+const dayjsPtbr = window.dayjs_locale_pt_br;
+const todaysDayNumber = dayjs().day();
+const weekDaysPtbr = dayjsPtbr.weekdays;
+const todaysDay = weekDaysPtbr[1];
+let exercisesDays = [];
 
-const exercises = require("../../exercises.json");
+const dayInfo = document.querySelector(".day-info"),
+  exercisesInfo = document.querySelector(".exercises-info");
+
+export function test() {
+  console.log(todaysDay);
+  console.log(exercises);
+}
 
 exercises.map((day) => {
-  console.table(`Dia da semana: ${day.dayName}, Exercícios: ${day.exercises}`);
+  exercisesDays.push(day.dayName);
 });
+
+export function checkExercisesDay() {
+  if (exercisesDays.includes(todaysDay)) {
+    showExercises();
+  } else {
+    dayInfo.innerText = "Hoje não é dia de exercícios.";
+  }
+}
+
+function showExercises() {
+  dayInfo.innerText = `Hoje é ${todaysDay}.`;
+  exercises.map((day) => {
+    for (let i = 0; i < day.exercises.length; i++) {
+      console.log(day.exercises[i].name);
+    }
+  });
+}
--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,17 @@
+import { checkExercisesDay } from "./exercises.js";
 import { startStopwatch, pauseStopwatch, stopStopwatch } from "./stopwatch.js";
 import { addTimerOptions, startTimer, pauseTimer, stopTimer } from "./timer.js";
 
 const stopwatchStartBtn = document.querySelector(".stopwatch .startBtn"),
   stopwatchPauseBtn = document.querySelector(".stopwatch .pauseBtn"),
-  stopwatchStopBtn = document.querySelector(".stopwatch .stopBtn"),
-  timerStartBtn = document.querySelector(".timer .startBtn"),
+  stopwatchStopBtn = document.querySelector(".stopwatch .stopBtn");
+
+const timerStartBtn = document.querySelector(".timer .startBtn"),
   timerPauseBtn = document.querySelector(".timer .pauseBtn"),
   timerStopBtn = document.querySelector(".timer .stopBtn");
+
+// ---------- Exercises ----------
+checkExercisesDay();
 
 // ---------- Stopwatch ----------
 stopwatchStartBtn.addEventListener("click", () => {
